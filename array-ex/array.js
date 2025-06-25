5ad0337f1cfcc195d8e247373f1b327d5d1c2fad
giorgiameffe <giorgiameffe03@gmail.com>
2025-06-25 12:49:51 +0200

    feat; complete find method exercise

--- array-ex/array.js
+++ array-ex/array.js
@@ -111,5 +111,24 @@ names.forEach((name) => {
     console.log(`Ciao, ${name}`);
 })
 
+// Esercizio 10: Metodo Find 
+
+// Dato un array di numeri [7, 3, 10, 5, 12, 8], utilizza il metodo find() per trovare il primo numero pari presente nell'array. 
+// Stampa il numero trovato o un messaggio se nessun numero pari è stato trovato.
+
+const numbersArray = [7, 3, 10, 5, 12, 8];
+
+const evenNumber = numbersArray.find(number => number % 2 === 0);
+
+if (evenNumber != undefined) {
+    console.log(evenNumber);
+} else {
+    console.log("Nessun numero pari presente")
+}
+
+
+
+
+
 
 
