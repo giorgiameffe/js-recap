9805845b4f30082b83120d032904ba4b73c1c203
giorgiameffe <giorgiameffe03@gmail.com>
2025-06-26 11:38:24 +0200

    feat: complete array objects exercise with for loop and filter method

--- array-objects-ex/arrayObjects.js
+++ array-objects-ex/arrayObjects.js
@@ -1,3 +1,5 @@
+// Esercizi con Oggetti
+
 // Esercizio 1: Creare un oggetto e accedere ai suoi valori
 
 // Crea un oggetto book con le seguenti proprietà:
@@ -86,3 +88,37 @@ const product = {
 
 delete product.code;
 console.log(product);
+
+// Esercizi con Array di Oggetti
+
+// Esercizio 1: Trovare oggetti specifici
+// Hai un array di prodotti. Trova tutti i prodotti con un prezzo inferiore a 50 euro e restituiscili in un nuovo array.
+
+const products = [
+    { id: 1, name: 'T-shirt', price: 25, category: 'Abbigliamento' },
+    { id: 2, name: 'Jeans', price: 70, category: 'Abbigliamento' },
+    { id: 3, name: 'Cuffie', price: 45, category: 'Elettronica' },
+    { id: 4, name: 'Libro', price: 15, category: 'Libri' },
+    { id: 5, name: 'Orologio', price: 120, category: 'Accessori' },
+    { id: 6, name: 'Mouse', price: 30, category: 'Elettronica' }
+]
+
+// metodo filter
+const cheapProducts = products.filter(product => product.price < 50);
+console.log(cheapProducts);
+
+// ciclo for 
+const newArrayProducts = [];
+
+for (let i = 0; i < products.length; i++) {
+
+    const currentProduct = products[i];
+    const currentProductPrice = products[i].price;
+
+    if (currentProductPrice < 50) {
+        newArrayProducts.push(currentProduct);
+    }
+
+}
+
+console.log(newArrayProducts);
