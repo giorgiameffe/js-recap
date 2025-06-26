5beed8b8e613c304f82cdfda8457862a5ed55f6f
giorgiameffe <giorgiameffe03@gmail.com>
2025-06-26 10:41:47 +0200

    feat: complete first array objects exercise

---
+++ array-objects-ex/arrayObjects.js
@@ -0,0 +1,18 @@
+// Esercizio 1: Creare un oggetto e accedere ai suoi valori
+
+// Crea un oggetto book con le seguenti proprietà:
+// title (stringa, es: "Il Signore degli Anelli")
+// author (stringa, es: "J.R.R. Tolkien")
+// publicationYear (numero, es: 1954)
+// Stampa in console il titolo del libro.
+// Stampa in console l'autore del libro usando la notazione con le parentesi quadre
+
+const book = {
+    title: "Le formidabili donne del Grand Hotel",
+    author: "Ruth Kvarnstorm-Jones",
+    publicationYear: 2024
+}
+
+console.log(book.title);
+console.log(book["author"]);
+
