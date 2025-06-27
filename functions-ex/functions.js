fb3fac1a78e648a000214ccf5953a8fdd7e95a83
giorgiameffe <giorgiameffe03@gmail.com>
2025-06-27 11:39:29 +0200

    feat: complete first function exercise for greeting users

---
+++ functions-ex/functions.js
@@ -0,0 +1,12 @@
+// Esercizio 1: Saluto Semplice
+
+// Crea una funzione chiamata hiUser che non accetta nessun parametro.
+// All'interno della funzione, stampa in console il messaggio: "Ciao! Benvenuto/a nel mondo delle funzioni."
+// Chiama la funzione.
+
+function hiUser() {
+    console.log('Ciao! Benvenuto/a nel mondo delle funzioni.')
+}
+
+hiUser();
+
