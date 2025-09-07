346a9b8fb11c91269d42e852731f5c30475eb936
giorgiameffe <giorgiameffe03@gmail.com>
2025-09-07 12:35:28 +0200

    feat: complete exercise four (filter)

--- array-ex/02-advanced/filter-method/script.js
+++ array-ex/02-advanced/filter-method/script.js
@@ -30,6 +30,7 @@ doubledNumbers.forEach((number) => {
 })
 
 // Esercizio 3: Filtra numeri grandi
+
 // Usa filter per creare un array con i numeri maggiori di 20.
 
 {
@@ -38,3 +39,27 @@ doubledNumbers.forEach((number) => {
     const filteredNumbers = numbers.filter(number => number > 20);
     console.log(filteredNumbers);
 }
+
+// Esercizio 4: Filtra parole lunghe
+
+// Dato un array di parole ["ciao", "javascript", "esercizio", "web", "fun"],
+// usa filter() per creare un nuovo array contenente solo le parole con più di 5 lettere.
+
+const words = ['ciao', 'javascript', 'esercizio', 'web', 'fun'];
+
+const filteredWords = words.filter(word => word.length > 5);
+console.log(filteredWords);
+
+
+// Esercizio 5: Numeri dispari elevati al quadrato
+
+// Crea un array di numeri da 1 a 15.
+// Usa filter() per ottenere solo i numeri dispari e poi map() per calcolare il quadrato di ciascun numero dispari.
+
+
+// Esercizio 6: Filtra oggetti per età e iniziale
+
+// Dato un array di oggetti {name, age} come:
+// [{name: "Alice", age: 17}, {name: "Anna", age: 25}, {name: "Bob", age: 30}, {name: "Carla", age: 20}]
+// usa filter() per creare un nuovo array con le persone che hanno più di 18 anni e il cui nome inizia con la lettera "A".
+
